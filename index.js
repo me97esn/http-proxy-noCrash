d994e0ddfe9b45a6d79ae807689c879652e77211
Emil Stenberg <esandin@gmail.com>
2014-04-14 10:22:20 +0200

    on error: log and set status

---
+++ index.js
@@ -0,0 +1,15 @@
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+require('colors');
+
+module.exports = {
+	web: function(req, res, options) {
+		console.log('Proxying using noCrash');
+
+		proxy.web(req, res, options, function(error){
+			console.log('An error occurred while proxying: '.redBG, error.message);
+			res.status(500);
+			res.end(error.message);
+		});
+	}
+}
