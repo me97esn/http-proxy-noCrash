b67cea9b043fed3a88aa8ddb919e0bab6ee0391d
Mikael Söderström <mikael.soderstrom@live.se>
2014-04-23 11:00:05 +0200

    Added the on property, which enables events from http-proxy

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require('colors');
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer();
-require('colors');
 
 module.exports = {
-	web: function(req, res, options) {
-		proxy.web(req, res, options, function(error){
-			console.log('An error occurred while proxying: '.redBG, error.message);
-			res.status(500);
-			res.end(error.message);
-		});
-	}
+    web: function (req, res, options) {
+        proxy.web(req, res, options, function (error) {
+            console.log('An error occurred while proxying: '.redBG, error.message);
+            res.status(500);
+            res.end(error.message);
+        });
+    },
+    on: function (event, callback) {
+        proxy.on(event, callback);
+    }
 }
