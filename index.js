b5467843f5288b483089e64035b99beaeccfe862
Emil Stenberg <esandin@gmail.com>
2014-05-07 12:49:55 +0200

    no crash has on error

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer();
 require('colors');
 
+proxy.on('error', function(e) {
+  console.log('An error occurred while proxying'.redBG, e.message)
+});
+
 module.exports = {
     web: function (req, res, options, callback) {
         proxy.web(req, res, options);
